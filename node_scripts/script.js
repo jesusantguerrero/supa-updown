594a587e0e97b8e2a7c9cebc7d817a275b0641d5
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-08-11 08:21:06 -0400

    feat: add response time in seconds

--- node_scripts/script.js
+++ node_scripts/script.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const { useSiteApi } = require("./utils/useApiServer");
 const { getAll, update } = useSiteApi();
+const { differenceInSeconds } = require('date-fns')
 
 exports.runBackground = () => {
     getAll().then(sites => {
@@ -10,10 +11,15 @@ exports.runBackground = () => {
 
 const updateCall = (site) => {
     const siteURL = `${site.protocol}${site.url}`;
-     axios.get(siteURL).then(response => {
+    const startTime = new Date();
+    axios.get(siteURL).then(response => {
+        const endTime = new Date();
+        const timeDiff = differenceInSeconds(startTime, endTime);
+
         const responseToSave = {
             status: response.status,
             statusText: response.statusText,
+            tesponseSeconds: timeDiff,
         }
         const responses = [...site.responses, responseToSave];
         update(site.id, { last_response: responseToSave, responses }).catch(error => {
