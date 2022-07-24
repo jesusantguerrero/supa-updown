bc344d9dada874c10b1d5bd4a2bb8ed805c82219
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-07-24 16:55:00 -0400

    Upgrade dependencies (#15)
    * 🚸 improve setup
    
    * 📝 update sql for post
    
    * update dependencies

--- functions/check-sites/check-sites.js
+++ functions/check-sites/check-sites.js
@@ -1,7 +1,7 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const { runBackground } = require('../../utils/runBackground');
 const handler = async () => {
-    await runBackground();
+    runBackground();
     return {
         statusCode: 200,
         body: "OK"
